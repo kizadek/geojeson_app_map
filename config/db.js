2f5c8d94d957d83afa19bacdf8a39889912c69b5
Mupela <48857035+kizadek@users.noreply.github.com>
2021-04-16 18:11:01 +0200

    init_102 🍿🍿

--- config/db.js
+++ config/db.js
@@ -1,16 +1,14 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({path:'./config/config.env'})
 require('colors');
 
-const connectDB = async () =>{
+ const connectDB = async () =>{
       const conn = await mongoose.connect(process.env.MONGO_URL,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
         useCreateIndex: true
       });
-
-      console.log(`MongoDB Connected host:${conn.connection.host}`.green);
-}
-
-module.exports = connectDB;
+      console.log(`MongoDB Connected host:${conn.connection.host}`.cyan);
+ }
+ module.exports = connectDB;
