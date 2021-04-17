029212e699e7eda278c28725ab255b13cdc37003
Mupela <48857035+kizadek@users.noreply.github.com>
2021-04-17 21:48:47 +0200

    init 105🍿🍿🍿

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,1 +1,14 @@
 const NodeGeocoder = require('node-geocoder');
+
+const options = {
+  provider: process.env.GEOCODER_PROVIDER,
+  
+  // Optionnal depending of the providers
+  httpAdapter: 'https', // Default
+  apiKey: process.env. GEOCODER_API_KEY, 
+  formatter: null         // 'gpx', 'string', ...
+};
+
+const geocoder = NodeGeocoder(options);
+
+module.exports = geocoder;
