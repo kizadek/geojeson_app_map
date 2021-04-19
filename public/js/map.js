4b85db4df135097eb1d7c639283a5a1ae8059172
Mupela <48857035+kizadek@users.noreply.github.com>
2021-04-19 10:38:48 +0200

    init 106🍿🍿🍿

---
+++ public/js/map.js
@@ -0,0 +1,9 @@
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibXVwZWxhIiwiYSI6ImNrbm16ZGdpaTB1NWQydnBybmpjM3U0cGkifQ.IZEw6pOVIVsuqlzk_kOY8w';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+zoom: 9,
+     center:[-71.157895,42.707741]
+});
